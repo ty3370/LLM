fd63667bd980c23ca724c7925a1a715a2d7e0edc
ty3370 <77711112+ty3370@users.noreply.github.com>
2024-12-14 11:39:06 +0900

    Add files via upload

--- inq_model01.py
+++ inq_model01.py
@@ -99,8 +99,7 @@ def page_1():
     st.session_state["user_number"] = st.text_input("학번", value=st.session_state["user_number"])
     st.session_state["user_name"] = st.text_input("이름", value=st.session_state["user_name"])
 
-    st.write("
-")  # Spacer to ensure button is at the bottom
+    st.write("")  # Spacer to ensure button is at the bottom
 if st.button("다음", key="unique_next_button"):
     if st.session_state["user_number"].strip() == "" or st.session_state["user_name"].strip() == "":
         st.error("학번과 이름을 모두 입력해주세요.")
